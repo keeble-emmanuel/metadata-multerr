8f04fc5dad53087c0ec533ad07190325525569aa
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-04-23 23:48:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
-
+const upload =multer({ dest : './uploads'})
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,6 +12,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  res.send("ok")
+})
 
 
 
